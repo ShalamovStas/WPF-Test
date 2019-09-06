003b7f1ce8df9c4b1bd35d2c2e12beb838655cb9
stas Shalamov <shalamov.stas1992@gmail.com>
2019-09-06 08:59:14 +0300

    Add project files.

---
+++ HorizontalList/CardsListcontrol.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HorizontalList
+{
+    /// <summary>
+    /// Interaction logic for CardsListcontrol.xaml
+    /// </summary>
+    public partial class CardsListcontrol : UserControl
+    {
+        public CardsListcontrol()
+        {
+            InitializeComponent();
+            var products = GetProducts();
+            if (products.Count > 0)
+                ListViewProducts.ItemsSource = products;
+        }
+
+        private List<Card> GetProducts()
+        {
+            var files = Directory.GetFiles("../../Assets", "*.png");
+            List<Card> cards = new List<Card>();
+            foreach (var filePath in files)
+            {
+                FileInfo fileInf = new FileInfo(filePath);
+                cards.Add(new Card(ParseFileName(fileInf.Name), fileInf.FullName));
+            }
+
+
+            return cards;
+        }
+
+
+        private string ParseFileName(string fileName)
+        {
+            return fileName.Remove(fileName.Length - 4);
+        }
+    }
+}
---
+++ HorizontalList/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Data;
+using System.Windows.Documents;
+
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HorizontalList
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+
+
+        private void ListViewMenu_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int index = ListViewMenu.SelectedIndex;
+
+            if (GridPrincipal == null)
+                return;
+
+            switch (index)
+            {
+                case 0:
+                    GridPrincipal.Children.Clear();
+                    GridPrincipal.Children.Add(new StartControl());
+                    break;
+                case 1:
+                    GridPrincipal.Children.Clear();
+                    GridPrincipal.Children.Add(new CardsListcontrol());
+                    break;
+                default:
+                    break;
+            }
+        }
+
+    }
+}
